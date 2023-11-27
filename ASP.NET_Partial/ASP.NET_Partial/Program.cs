0385f382fb21ad4f9664c34bef934218b820c196
SnnBrgmv <sananai@code.edu.az>
2023-11-27 17:36:43 +0400

    ASP.NET Multiple LAyout and Partial View

---
+++ ASP.NET_Partial/ASP.NET_Partial/Program.cs
@@ -0,0 +1,15 @@
+namespace ASP.NET_Partial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
